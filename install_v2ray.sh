141491b1537483f5ea745a1ad74d4b863b9696e4
zhangpeng <zhangpeng.fic@163.com>
2021-09-15 13:05:54 +0800

    安装脚本

---
+++ install_v2ray.sh
@@ -0,0 +1,15 @@
+#unzip v2ray-linux-64.zip
+#cd v2ray-linux-64
+#
+#mkdir -p /etc/v2ray /usr/bin/v2ray /var/log/v2ray # 创建目录
+#chmod +x v2ray v2ctl # 赋予可执行权限
+#cp -rf v2ray v2ctl geoip.dat geosite.dat -t /usr/bin/v2ray # 移动文件
+#cp -rf vpoint_vmess_freedom-try.json /etc/v2ray/config.json #仅创建配置文件
+#cp -rf systemd/v2ray.service /etc/systemd/system/ # 移动文件
+#systemctl enable v2ray # 用于程序开机启动并且崩溃时自动重启程序
+#systemctl restart v2ray
+
+rm -rf ./temp
+git clone https://github.com/zhangpengfic/v2ray_model -b "main" ./temp --depth=1
+
+
